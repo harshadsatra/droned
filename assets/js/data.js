29dbe1d886dbed38d1aac765047202ed0245d64d
Harshad <harshad.satra2@gmail.com>
2020-10-18 17:14:13 +0530

    Droned

---
+++ assets/js/data.js
@@ -0,0 +1,128 @@
+var data=[
+ {
+   "id": 1,
+   "year": 2014,
+   "type": "hd",
+   "count": 4
+ },
+ {
+   "id": 2,
+   "year": 2014,
+   "type": "nhd",
+   "count": 2
+ },
+ {
+   "id": 3,
+   "year": 2014,
+   "type": "gov",
+   "count": 6
+ },
+ {
+   "id": 4,
+   "year": 2015,
+   "type": "hd",
+   "count": 20
+ },
+ {
+   "id": 5,
+   "year": 2015,
+   "type": "nhd",
+   "count": 13
+ },
+ {
+   "id": 6,
+   "year": 2015,
+   "type": "gov",
+   "count": 17
+ },
+ {
+   "id": 7,
+   "year": 2016,
+   "type": "hd",
+   "count": 19
+ },
+ {
+   "id": 8,
+   "year": 2016,
+   "type": "nhd",
+   "count": 3
+ },
+ {
+   "id": 9,
+   "year": 2016,
+   "type": "gov",
+   "count": 3
+ },
+ {
+   "id": 10,
+   "year": 2017,
+   "type": "hd",
+   "count": 23
+ },
+ {
+   "id": 11,
+   "year": 2017,
+   "type": "nhd",
+   "count": 15
+ },
+ {
+   "id": 12,
+   "year": 2017,
+   "type": "gov",
+   "count": 29
+ },
+ {
+   "id": 13,
+   "year": 2018,
+   "type": "hd",
+   "count": 16
+ },
+ {
+   "id": 14,
+   "year": 2018,
+   "type": "nhd",
+   "count": 10
+ },
+ {
+   "id": 15,
+   "year": 2018,
+   "type": "gov",
+   "count": 35
+ },
+ {
+   "id": 16,
+   "year": 2019,
+   "type": "hd",
+   "count": 35
+ },
+ {
+   "id": 17,
+   "year": 2019,
+   "type": "nhd",
+   "count": 15
+ },
+ {
+   "id": 18,
+   "year": 2019,
+   "type": "gov",
+   "count": 11
+ },
+ {
+   "id": 19,
+   "year": 2020,
+   "type": "hd",
+   "count": 43
+ },
+ {
+   "id": 20,
+   "year": 2020,
+   "type": "nhd",
+   "count": 20
+ },
+ {
+   "id": 21,
+   "year": 2020,
+   "type": "gov",
+   "count": 7
+ }
+];
